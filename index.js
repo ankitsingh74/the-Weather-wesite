ba4c7c6074d6dabb7c3f1fd2edef0273e33829c1
Ankit Kumar <ankitroy2292@gmail.com>
2023-03-18 18:10:44 +0530

    second commit

--- index.js
+++ index.js
@@ -37,7 +37,6 @@ form.addEventListener('submit', (e)=>{
         alert("Please type in city name");
     }else{
         cityInput = search.value;
-        console.log(cityInput);
         fetchWeatherData();
 
         search.value = "";
@@ -47,35 +46,45 @@ form.addEventListener('submit', (e)=>{
     e.preventDefault();
 });
 
-
 function dayofTheWeek(day, month, year){
-    const weekday = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-    ];
-    return weekday[new Date(`${day}/${month}/${year}`).getDay()];
-};
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+    // return weekday[new Date(`${day}/${month}/${year}`).getDay()];
+    return weekday[new Date(day,month,year).getDay()]
+}
+
+// console.log(dayofTheWeek(16, 03, 2023));
+
+
+
+//  function dayofTheWeek(day, month, year){
+//     const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+//     // console.log(weekday[0])
+
+//     return weekday[new Date(`${day}/${month}/${year}`).getDay()];
+// };
+// console.log("function call    ---------------------",dayofTheWeek())
 //function  that fetches and displays the data and add dynamically the city name with template literals
 //  const APIkey = "cf84f5d15a93485794143559230603" 
 
- function fetchWeatherData(){
-    fetch(`http://api.weatherapi.com/v1/current.json?key=041efc35449d4a8fa0e44002230603&q=${cityInput}`).then(response => response.json()).then(data => {
-        // console.log(data);
+  function fetchWeatherData(){
+     fetch(`http://api.weatherapi.com/v1/current.json?key=041efc35449d4a8fa0e44002230603&q=${cityInput}`).then(response => response.json())
+    .then(data => {
+            temp.innerHTML = data.current.temp_c + "&#176;";
 
-        temp.innerHTML = data.current.temp_c + "&#176;";
         conditionOutput.innerHTML = data.current.condition.text;
 
         const date = data.location.localtime;
-        const y = parseInt(date.substr(0,4));
-        const m = parseInt(date.substr(5,2));
-        const d = parseInt(date.substr(8,2));
-        const time = date.substr(11);
         console.log(data);
+        const y = parseInt(date.substr(0,4));
+        const m = (+date.substr(5,2));
+        const d = parseInt(date.substr(8,2));
+        console.log(data.current.temp_c);
+    //     console.log(typeof(m));
+    //    console.log("y"+y);
+    //    console.log("m"+m);
+    //    console.log("d"+d);
+    //    console.log("d"+date);
+        const time = date.substr(11);
         
         dateOutput.innerHTML = `${dayofTheWeek(d,m,y)} ${d}, ${m} ${y}`;
         timeOutput.innerHTML = time; 
@@ -89,12 +98,13 @@ function dayofTheWeek(day, month, year){
         humidityOutput.innerHTML = data.current.humidity + "%";
         windOutput.innerHTML = data.current.wind_kph + "km/h";
         
+
         let timeOfDay = "day"
         const code = data.current.condition.code;
 
         if(!data.current.is_day){
             timeOfDay = "night";
-        
+        }
 
         if(code == 1000){
             app.style.backgroundImage = `url(./images/${timeOfDay}/clear.jpg)`;
@@ -103,27 +113,27 @@ function dayofTheWeek(day, month, year){
                 btn.style.background = "#181e27";
             }
             
-        }
-    }else if(
+        
+        }else if(
         code == 1003 ||
         code == 1006 ||
         code == 1009 ||
         code == 1030 ||
+        code == 1063 ||
         code == 1069 ||
         code == 1087 ||
         code == 1135 ||
         code == 1273 ||
-        code == 1276 ||
         code == 1279 ||
         code == 1282
-    ){
-        app.style.backgroundImage = `
-        url(.images/${timeOfDay/cloudy.jpg})`;
+        )
+        {
+        app.style.backgroundImage = `url(./images/${timeOfDay}/cloudy.jpg)`;
         btn.style.background = "#fa6d1b";
         if(timeOfDay == "night"){
             btn.style.background = "#181e27";
         }
-    }else if(
+        }else if(
         code == 1003 ||
         code == 1006 ||
         code == 1009 ||
@@ -131,31 +141,36 @@ function dayofTheWeek(day, month, year){
         code == 1069 ||
         code == 1087 ||
         code == 1135 ||
+        code == 1183 ||
         code == 1273 ||
         code == 1276 ||
+        code == 1246 ||
         code == 1279 ||
+        code == 1240 ||
         code == 1282
-    ){
+        )
+        {
         app.style.backgroundImage = `url(./images/${timeOfDay}/rainy.jpg)`;
         btn.style.background = "#647d75";
         if(timeOfDay == "night"){
             btn.style.background = "#325c80";
        
         }
-    }else{
+        }else{
         app.style.backgroundImage = `url(./images/${timeOfDay}/snowy.jpg)`;
         btn.style.background = "#4d72aa";
         if(timeOfDay == "night"){
             btn.style.background = "#1b1b1b";
         }
-    }
+        }
 
-    app.style.opacity = "1";
-    })
-    .catch(() => {
-        alert("City Not Found, Please try again");
+            app.style.opacity = "1";
+        })
+    .catch((e) => {
+       alert(e)
         app.style.opacity = "1";
     });
+    
 
  }
 
